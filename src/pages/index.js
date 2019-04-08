68d9f7e792245e3aaf9d15456ddf568a997ff46a
Faraz <farazuddinva@gmail.com>
2019-04-07 23:03:00 -0400

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,30 +7,21 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => ( <
-  Layout >
-  <
-  SEO title = "Home"
-  keywords = {
-    [`gatsby`, `application`, `react`]
-  }
-  /> <
-  h1 > Learn to design and code React apps < /h1> <
-  p > Complete courses about the best tools and design systems.Prototype and build apps with React and FramerX. < /p>
+const IndexPage = () =>
+  (<Layout >
+    <SEO title="Home" keywords={
+      [`gatsby`, `application`, `react`]
+    } />
 
-  <
-  div style = {
-    {
-      maxWidth: `300px`,
-      marginBottom: `1.45rem`
-    }
-  } >
-  <
-  Image / >
-  <
-  /div> <
-  Link to = "/page-2/" > Watch the video < /Link> < /
-  Layout >
-)
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1 > Faraz Uddin's First Ever React Website< /h1>
+      <p> I really enjoy Design + Code. My goal is to become a Full-Stack UX/UI Designer< /p>
+      <Link to="/page-2/" > Watch the video < /Link> 
+    </div>
+  </div>
 
+
+          < /Layout > )
+                
 export default IndexPage
